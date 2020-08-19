3a67343621ec3bb0f6f5c0986de01941132e6324
Rajat Gupta <rajatgupta828@gmail.com>
2020-08-20 00:56:43 +0530

    faster Running Consumers

--- kafka-consumer-elasticSearch/src/main/java/com/rajat/example/ElasticSearchComsumer.java
+++ kafka-consumer-elasticSearch/src/main/java/com/rajat/example/ElasticSearchComsumer.java
@@ -1,5 +1,6 @@
 package com.rajat.example;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -12,6 +13,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.*;
@@ -34,10 +37,16 @@ public class ElasticSearchComsumer {
 
        //Create a consumer
         KafkaConsumer<String, String> myConsumer = createConsumer("twitter_tweets");
+
+        //Create a bulk request :
+        BulkRequest bulkRequest = new BulkRequest();
         // Read/Poll the data
         while(true){
             ConsumerRecords<String,String> recordsRead =  myConsumer.poll(Duration.ofMillis(100));
 
+            Integer recordsReadCount = recordsRead.count();
+
+            logger.info("Records : " + recordsReadCount + " read");
             for(ConsumerRecord<String, String> records : recordsRead){
                 // Insert data into elastic search
                 String jsonString = records.value();
@@ -45,22 +54,32 @@ public class ElasticSearchComsumer {
                 // To make the consumer Idempotent, we have 2 strategies which we re going to define below:
 
                 // Strategy 1: Kafka Generic ID
-                String id1 = records.topic() + "_" + records.partition() + "_" + records.offset();
+                //String id1 = records.topic() + "_" + records.partition() + "_" + records.offset();
 
                 //Strategy 2 - Use the incoming ID like in case of twitter we can use a new ID from the payload
+                try{
+                    String id1 = getIdFromData(records.value());
+                    IndexRequest indexRequest = new IndexRequest("twitter","_doc",
+                            id1).source(jsonString, XContentType.JSON);
+                    bulkRequest.add(indexRequest);
+                }catch (Exception e){
+                    e.printStackTrace();
+                    logger.warn("Skipping Bad Data : " + records.value());
+                }
+            }
 
-                logger.info("Kafka Generic ID : " + id1);
+            if (recordsReadCount > 0){
+                BulkResponse bulkItemResponses = clients.bulk(bulkRequest, RequestOptions.DEFAULT);
+                logger.info("Committing the records...");
+                //Performing the commits
+                myConsumer.commitSync();
 
-                IndexRequest indexRequest = new IndexRequest("twitter",
-                        id1).source(jsonString, XContentType.JSON);
+                logger.info("Commited successfully");
 
-                IndexResponse indexResponse = clients.index(indexRequest, RequestOptions.DEFAULT);
-                String id = indexResponse.getId();
-                logger.info(id);
-                logger.info(jsonString);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
+
                     e.printStackTrace();
                 }
             }
@@ -82,6 +101,9 @@ public class ElasticSearchComsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "200");
+
 
 
         //Create the consumer
@@ -115,6 +137,12 @@ public class ElasticSearchComsumer {
 
         RestHighLevelClient client = new RestHighLevelClient(builder);
         return client;
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+
+    private static String getIdFromData(String tweetJson){
+        return  jsonParser.parse(tweetJson).getAsJsonObject().get("id_str").getAsString();
     }
 }
 
